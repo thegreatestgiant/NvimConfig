cd367733cd305cb58232decb50ccd450784358b1
= <=>
2024-04-03 16:24:16 -0400

    :heavy_plus_sign: Add yanky and mini.move

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,50 +1,49 @@
 return {
-  "echasnovski/mini.nvim",
-  config = function()
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
-    require("mini.surround").setup()
+	"echasnovski/mini.nvim",
+	config = function()
+		-- Add/delete/replace surroundings (brackets, quotes, etc.)
+		--
+		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+		-- - sd'   - [S]urround [D]elete [']quotes
+		-- - sr)'  - [S]urround [R]eplace [)] [']
+		require("mini.surround").setup()
+		require("mini.pairs").setup()
+		require("mini.move").setup()
+		require("mini.bufremove").setup()
+		require("utils").load_mappings("bufremove")
 
-    require("mini.pairs").setup()
+		require("mini.comment").setup({
+			custom_commentstring = function()
+				return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+			end,
+		})
 
-    require("mini.bufremove").setup()
-    require("utils").load_mappings("bufremove")
-
-    require("mini.comment").setup({
-      custom_commentstring = function()
-        return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-      end,
-    })
-
-    require("mini.indentscope").setup({
-      symbol = "▏",
-      options = {
-        try_as_border = true,
-      },
-    })
-  end,
-  init = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = {
-        "help",
-        "alpha",
-        "dashboard",
-        "neo-tree",
-        "NvimTree",
-        "Trouble",
-        "trouble",
-        "lazy",
-        "mason",
-        "notify",
-        "toggleterm",
-        "lazyterm",
-      },
-      callback = function()
-        vim.b.miniindentscope_disable = true
-      end,
-    })
-  end,
+		require("mini.indentscope").setup({
+			symbol = "▏",
+			options = {
+				try_as_border = true,
+			},
+		})
+	end,
+	init = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"help",
+				"alpha",
+				"dashboard",
+				"neo-tree",
+				"NvimTree",
+				"Trouble",
+				"trouble",
+				"lazy",
+				"mason",
+				"notify",
+				"toggleterm",
+				"lazyterm",
+			},
+			callback = function()
+				vim.b.miniindentscope_disable = true
+			end,
+		})
+	end,
 }
