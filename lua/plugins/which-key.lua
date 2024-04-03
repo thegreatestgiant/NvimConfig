1a48ba555cf364ac3e354b52fa43f9dcfdfb79a4
= <=>
2024-04-03 15:04:47 -0400

    :heavy_plus_sign: Add flash and also which-key stuff

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,8 +1,21 @@
 return {
-  "folke/which-key.nvim",
-  event = "VimEnter",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
+	"folke/which-key.nvim",
+	event = "VimEnter",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	opts = {
+		plugins = { spelling = true },
+		defaults = {
+			mode = { "n", "v" },
+			["<leader>f"] = { name = "+find" },
+			["<leader>t"] = { name = "+trouble" },
+		},
+	},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
+		wk.register(opts.defaults)
+	end,
 }
